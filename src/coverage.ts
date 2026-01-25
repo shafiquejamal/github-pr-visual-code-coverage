684e330a396f8e9b7c8072d7759e8734cb4430f0
gelocraft <gelocraft@users.noreply.github.com>
2026-01-25 18:26:12 +0800

    chore: update highlight function

--- src/coverage.ts
+++ src/coverage.ts
@@ -5,29 +5,33 @@ const Coverage: ICodeCoverage = {
 		const coverage_json = await coverageArtifact.toJson()
 
 		coverage_json.forEach(({ filename, covered_lines, uncovered_lines }) => {
-			const file = document.querySelector(`[data-file-path="${filename}"]`)
-			if (file) {
-				covered_lines.forEach(line => {
-					file
-						.querySelectorAll(`td[data-line-number="${line}"]`)
-						.forEach(e => e.classList.add('covered-lines'))
-				})
-				uncovered_lines.forEach(line => {
-					file.querySelectorAll(`td[data-line-number="${line}"]`).forEach(e => {
-						e.classList.remove('covered-lines')
-						e.classList.add('uncovered-lines')
-					})
-				})
-			}
-		})
+			const lineElements = document.querySelectorAll(
+				`table[aria-label="Diff for: ${filename}"] > tbody > tr > td:nth-child(2)`
+			)
 
-		document.querySelectorAll('td.blob-num.blob-num-expandable').forEach(e =>
-			e.addEventListener('click', () => {
-				setTimeout(() => {
-					this.highlight(coverageArtifact)
-				}, 1000)
+			console.log(`${filename}, covered_lines =`, covered_lines)
+			console.log(`${filename}, uncovered_lines =`, uncovered_lines)
+
+			lineElements.forEach(lineElement => {
+				const code: Element | null = lineElement.querySelector('code')
+				if (code?.textContent) {
+					const lineNumber = Number(code.textContent)
+
+					covered_lines.forEach(covered_line => {
+						if (covered_line === lineNumber) {
+							lineElement.classList.add('covered-lines')
+						}
+					})
+
+					uncovered_lines.forEach(uncovered_line => {
+						if (uncovered_line === lineNumber) {
+							lineElement.classList.remove('covered-lines')
+							lineElement.classList.add('uncovered-lines')
+						}
+					})
+				}
 			})
-		)
+		})
 	}
 }
 
