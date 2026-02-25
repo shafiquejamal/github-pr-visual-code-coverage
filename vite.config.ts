772c42aa8412262dc72b7139f7b8df9cf46ddef4
GeloCraft <115651305+gelocraft@users.noreply.github.com>
2026-02-26 00:27:44 +0800

    enhancement: update credentials user interface (#12)
    Co-authored-by: gelocraft <gelocraft@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,8 @@ export default defineConfig({
 			input: {
 				background: path.resolve(__dirname, 'src/scripts/background.ts'),
 				content: path.resolve(__dirname, 'src/scripts/content.ts'),
-				popup: path.resolve(__dirname, 'src/scripts/popup.ts')
+				popup: path.resolve(__dirname, 'src/scripts/popup.ts'),
+				credentials: path.resolve(__dirname, 'src/scripts/credentials.ts')
 			},
 			output: {
 				entryFileNames: 'scripts/[name].js',
